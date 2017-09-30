2ad6478ba24ea64ca7565808a3dfbba4f28dbee7
Anwesh43 <anweshthecool0@gmail.com>
2017-09-30 22:40:04 +0530

    making button smaller in size

--- public/RealtimeNotificationComponent.js
+++ public/RealtimeNotificationComponent.js
@@ -41,20 +41,22 @@ class NotiticationButton {
         this.n = 0
     }
     draw(context,image) {
-        const n = this.n
+        const n = this.n-1 + (this.state.dir+1)/2
         context.save()
         context.translate(this.w/2,this.h/2)
         context.drawImage(image,-image.width/2,-image.height/2)
-        if(n != 0) {
-            console.log(this.n)
+        if(this.n != 0) {
+            context.save()
+            context.scale(this.state.scale,this.state.scale)
             context.beginPath()
-            context.arc(this.w*0.2,-this.h*0.2,(0.1*this.w*this.state.scale),0,2*Math.PI)
+            context.arc(this.w*0.2,-this.h*0.2,(0.15*this.w),0,2*Math.PI)
             context.fillStyle = '#f44336'
             context.fill()
-            context.font = context.font.replace(/\d{2}/,this.w/8)
+            context.font = context.font.replace(/\d{2}/,this.w/6)
             const text = `${n}`
             context.fillStyle = 'white'
             context.fillText(text,this.w*0.2-context.measureText(text).width/2,this.w/20-this.h*0.2)
+            context.restore()
         }
         context.restore()
     }
